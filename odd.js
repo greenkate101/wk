3672abfb384ffaaaa02a29bf6da00c1906f43218
Kate Green <greenkate@me.com>
2016-10-03 20:42:07 -0400

    worked on

---
+++ odd.js
@@ -0,0 +1,14 @@
+var i = 50;
+
+while (i >= -50) {
+		if (i === 0) {
+			console.log(i);
+		} else {
+			if (i % 2 === 0) {
+			console.log('even' + i);
+		} else { 
+			console.log('odd' + i);
+		}
+	}
+	i--;
+}
